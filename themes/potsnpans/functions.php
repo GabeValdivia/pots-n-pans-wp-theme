21a254088162210224cf4f76f404d66c0186fd64
Gabe Valdivia <sinaelval@gmail.com>
2022-04-21 20:34:53 -0500

    Enqueue main.css in funcitons.php. Get SASS Live Server running

--- themes/potsnpans/functions.php
+++ themes/potsnpans/functions.php
@@ -139,6 +139,8 @@ add_action( 'widgets_init', 'potsnpans_widgets_init' );
  */
 function potsnpans_scripts() {
 	wp_enqueue_style( 'potsnpans-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'potsnpans-main', get_template_directory_uri()  . '/css/main.css' );
+
 	wp_style_add_data( 'potsnpans-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'potsnpans-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
